f90be50e2dd18a5fbb8b67dbdfc7807667f4da90
Stepan Pijacek <stepan.pijacek@gmail.com>
2022-04-24 11:04:27 +0200

    File reformating and solving call issues.

--- MAI/Utils/CreateTrainingData.py
+++ MAI/Utils/CreateTrainingData.py
@@ -1,8 +1,8 @@
-from MAI.Utils.ReadData.ReadData import prepareDataset
+from MAI.Utils.ReadData.ReadData import main
 
 
 def training_function():
-    train_df, test_df, all_labels = prepareDataset()
+    train_df, test_df, all_labels = main()
     # train_df['path'] = train_df['path'].astype(str)
     # train_df.rename(columns={'OriginalImagePixelSpacing[x,y] ': 'OriginalImagePixelSpacing[x,y]'})
     return train_df, test_df, all_labels

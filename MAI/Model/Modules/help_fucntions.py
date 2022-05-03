f7c7c922b7c4af0272c94b4bf0cf625c74e3e5de
Stepan Pijacek <stepan.pijacek@gmail.com>
2022-05-03 21:31:42 +0200

    New architecture and code clean up

--- MAI/Model/Modules/help_fucntions.py
+++ MAI/Model/Modules/help_fucntions.py
@@ -129,10 +129,7 @@ class CapsuleLayer(layers.Layer):
         # Regard the first two dimensions as `batch` dimension, then
         # matmul(W, x): [..., dim_capsule, input_dim_capsule] x [..., input_dim_capsule, 1] -> [..., dim_capsule, 1].
         # inputs_hat.shape = [None, num_capsule, input_num_capsule, dim_capsule]
-        try:
-            inputs_hat = tf.squeeze(tf.map_fn(lambda x: tf.matmul(self.W, x), elems=inputs_tiled))
-        except Exception as e:
-            print(str(e))
+        inputs_hat = tf.squeeze(tf.map_fn(lambda x: tf.matmul(self.W, x), elems=inputs_tiled))
 
         # Begin: Routing algorithm ---------------------------------------------------------------------#
         # The prior for coupling coefficient, initialized as zeros.

47f583d23c257124361abff0704cab49b977682e
Stepan Pijacek <stepan.pijacek@gmail.com>
2022-05-10 16:31:00 +0200

    New architecture and code clean up

--- MAI/Model/Modules/help_fucntions.py
+++ MAI/Model/Modules/help_fucntions.py
@@ -147,6 +147,7 @@ class CapsuleLayer(layers.Layer):
         for i in range(self.routings):
             # c.shape=[batch_size, num_capsule, 1, input_num_capsule]
             c = tf.nn.softmax(b, axis=-1)
+            print("\n printing C")
             print(c)
             # c.shape = [batch_size, num_capsule, 1, input_num_capsule]
             # inputs_hat.shape=[None, num_capsule, input_num_capsule, dim_capsule]

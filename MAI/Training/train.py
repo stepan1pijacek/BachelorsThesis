2f891e33452405745816d451f83a46c6f4d2fed4
Stepan Pijacek <stepan.pijacek@gmail.com>
2022-05-15 08:01:30 +0200

    New validation split value

--- MAI/Training/train.py
+++ MAI/Training/train.py
@@ -23,7 +23,7 @@ class TrainingClass:
             shear_range=0.1,
             fill_mode='reflect',
             zoom_range=0.2,
-            validation_split=0.2)
+            validation_split=0.3)
 
         train_gen = core_idg.flow_from_dataframe(dataframe=train_df,
                                                  directory=None,

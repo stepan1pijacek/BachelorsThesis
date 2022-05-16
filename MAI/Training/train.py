ca411aed951fa3d345a4badfb9fc2567f26be334
Stepan Pijacek <stepan.pijacek@gmail.com>
2022-05-16 17:39:47 +0200

    New branch with binary cross entropy instead of AsyncLossOptimized

--- MAI/Training/train.py
+++ MAI/Training/train.py
@@ -25,7 +25,7 @@ class TrainingClass:
             shear_range=0.1,
             fill_mode='reflect',
             zoom_range=0.2,
-            validation_split=0.255)
+            validation_split=0.2559)
 
         train_gen = core_idg.flow_from_dataframe(dataframe=train_df,
                                                  directory=None,

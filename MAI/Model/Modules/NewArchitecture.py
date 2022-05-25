3f838bcc05026d9ef56e0f39be4c9c04a572e7df
Stepan Pijacek <stepan.pijacek@gmail.com>
2022-05-25 18:44:59 +0200

    Added efficient net and inception net into global view branch

--- MAI/Model/Modules/NewArchitecture.py
+++ MAI/Model/Modules/NewArchitecture.py
@@ -26,9 +26,13 @@ def global_view(model):
 
 
 def capsNet_view(model, n_class, routings):
-    res_net = ResNet152V2(include_top=False)(model)
+    res_net = ResNet152V2(include_top=False, weights='imagenet')(model)
+
     primaryCaps = PrimaryCap(res_net, dim_capsule=4, n_channels=8, kernel_size=9, strides=2, padding='valid')
-    digitCaps = CapsuleLayer(num_capsule=n_class, dim_capsule=16, routings=routings, name='digitcaps')(primaryCaps)
+    digitCaps = CapsuleLayer(num_capsule=n_class, dim_capsule=8, routings=routings, name='digitcaps')(primaryCaps)
+    digitCaps = CapsuleLayer(num_capsule=n_class + 2, dim_capsule=16, routings=routings + 2, name='digitcaps2')(digitCaps)
+    digitCaps = CapsuleLayer(num_capsule=n_class, dim_capsule=8, routings=routings, name='digitcaps3')(digitCaps)
+
     out_caps = Length(name='capsnet')(digitCaps)
     out_caps = Dense(128)(out_caps)
     out_caps = Dropout(0.5)(out_caps)
@@ -42,10 +46,12 @@ def embedded_models(input_shape=(IMG_SIZE, IMG_SIZE, 3),
                     batch_size_o=BATCH_SIZE):
     input = Input(shape=input_shape, batch_size=batch_size_o)
 
-    model = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(input)
-    model = Activation('relu')(model)
+    model = Conv2D(64, (5, 5), activation='relu', padding='same', kernel_initializer='he_normal')(input)
+    model = BatchNormalization()(model)
+    model = Conv2D(64, (5, 5), activation='relu', padding='same')(model)
+    model = Conv2D(128, (3, 3), activation='relu')(model)
+    model = Conv2D(128, (3, 3), activation='relu')(model)
 
-    model = Conv2D(3, 1, strides=1, padding='same')(model)
     gv = global_view(model)
     cnv = capsNet_view(model, n_class, routings)
 

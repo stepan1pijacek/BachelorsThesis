69f0e3e88daba8c92a1a4c2fb7ef7a60b907da63
Stepan Pijacek <stepan.pijacek@gmail.com>
2022-05-26 19:51:04 +0200

    Added secondary ResNet with 50 layers

--- MAI/Model/Modules/NewArchitecture.py
+++ MAI/Model/Modules/NewArchitecture.py
@@ -16,25 +16,34 @@ def global_view(model):
     efficient = GlobalAveragePooling2D()(efficient)
     efficient = Dense(256)(efficient)
     efficient = Dropout(0.5)(efficient)
-    efficient = Dense(16)(efficient)
-    return efficient
+    efficient = Dense(14)(efficient)
+
+    resnet_fifty = ResNet50V2(include_top=False)(model)
+    resnet_fifty = GlobalAveragePooling2D()(resnet_fifty)
+    resnet_fifty = Dense(256)(resnet_fifty)
+    resnet_fifty = Dropout(0.5)(resnet_fifty)
+    resnet_fifty = Dense(14)(resnet_fifty)
+    return efficient, resnet_fifty
 
 
 def capsNet_view(input, routings):
-    conv1 = Conv2D(filters=256, kernel_size=9, strides=1, padding='valid', activation='relu', name='conv1')(input)
+    x = Conv2D(64, (5, 5), activation='relu')(input)
+    x = BatchNormalization()(x)
+    x = Conv2D(64, (5, 5), activation='relu')(x)
+    x = Conv2D(128, (3, 3), activation='relu')(x)
+    x = Conv2D(128, (3, 3), activation='relu')(x)
 
-    # Layer 2: Conv2D layer with `squash` activation, then reshape to [None, num_capsule, dim_vector]
-    primarycaps = PrimaryCap(conv1, dim_capsule=8, n_channels=64, kernel_size=9, strides=2, padding='valid')
+    primarycaps = PrimaryCap(x, dim_capsule=2, n_channels=8, kernel_size=7, strides=2, padding='valid')
 
     # Layer 3: Capsule layer. Routing algorithm works here.
-    digitcaps = CapsuleLayer(num_capsule=14, dim_capsule=16, routings=routings, name='digitcaps')(primarycaps)
+    digitcaps = CapsuleLayer(num_capsule=16, dim_capsule=8, routings=routings, name='digitcaps')(primarycaps)
     digitcaps = CapsuleLayer(num_capsule=14, dim_capsule=4, routings=routings, name='digitcaps2')(digitcaps)
 
     # Layer 4: This is an auxiliary layer to replace each capsule with its length. Just to match the true label's shape.
     # If using tensorflow, this will not be necessary. :)
     out_caps = Length(name='capsnet')(digitcaps)
 
-    return Concatenate[out_caps]
+    return out_caps
 
 
 def embedded_models(input_shape=(IMG_SIZE, IMG_SIZE, 3),
@@ -43,10 +52,10 @@ def embedded_models(input_shape=(IMG_SIZE, IMG_SIZE, 3),
                     batch_size_o=BATCH_SIZE):
     input = Input(shape=input_shape, batch_size=batch_size_o)
 
-    gv_efficient = global_view(input)
+    gv_efficient, gv_fifty = global_view(input)
     cnv = capsNet_view(input, routings)
 
-    fusion = concatenate([gv_efficient, cnv])
+    fusion = concatenate([gv_efficient, gv_fifty, cnv])
 
     fusion = Dense(32)(fusion)
     fusion = Dropout(0.2)(fusion)
